b2dec364ca48f362d6d214636535c6abf0044141
henriquegalisa <henriquegalisa@outlook.com>
2021-05-16 18:31:09 -0300

    entrega 2.3

---
+++ src/app/local/local.page.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { LocalPage } from './local.page';
+
+describe('LocalPage', () => {
+  let component: LocalPage;
+  let fixture: ComponentFixture<LocalPage>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LocalPage ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LocalPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/local/local.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-local',
+  templateUrl: './local.page.html',
+  styleUrls: ['./local.page.scss'],
+})
+export class LocalPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
